7a95de81e5159bf60785c69943d7f256031a3592
kazu258078 <32408485+kazu258078@users.noreply.github.com>
2019-03-11 20:17:17 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var knex = require('knex')({
+  dialect:'mysql',
+  connection:{
+    host:'',
+    user:'',
+    password:'',
+    database:'',
+    charset:'utf8'
+  }
 });
+var Bookshelf = require('bookshelf')(knex);
+Bookshelf.plugin('pagenation');
+
+var User = Bookshelf.Model.extend({
+  tableName:'users'
+});
+
+var Message = Bookshelf.Model.extend({
+  tableName:'messages',
+  hasTimestamps:true,
+  user:function(){
+    return this.belongsTo(User);
+  }
+});
+
+router.get('/', function(req, res, next) {
+  if(req.session.login == null){
+    res.redirect('/users');
+  }else{
+    res.redirect('/1');
+  }
+});
+
+router.get('/', function(req, res, next) {
+   if(req.session.login == null){
+    res.redirect('/users');
+     return;
+  }
+  
+  var pg*=1;
+  if(pg < 1){
+    pg=1;
+  }
+  new Message().orderBy('created_at','DESC')
+    .fetchPage({page:pg,pageSize:10,withRelated:['users']})
+    .then((collection)=>{
+      var data ={
+        title:'miniboard',
+        login:req.session.login,
+        collection:collection.toArray(),
+        pagination:collection.pagination
+      };
+    res.render('index',data);
+  }).catch((err)=>{
+    res.status(500).json({error:true,data:{message:err.message}});
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var rec ={
+    message:req.body.msg,
+    user_id:req.session.login.id
+  }
+  new Message(rec).save().then((model)=>{
+    res.redirect('/');
+  })
+});
+
 
 module.exports = router;
