99c3bafd3ea19e1fb7e42347f2bf6d649f792ff5
kazu258078 <32408485+kazu258078@users.noreply.github.com>
2019-03-13 15:07:31 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -24,7 +24,7 @@ var Message = Bookshelf.Model.extend({
   tableName:'messages',
   hasTimestamps: true,
   user: function(){
-    return this.belongTo(User)
+    return this.belongsTo(User);
   }
 });
 
